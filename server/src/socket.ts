a5d55e9288a7ed8284e72fbb36971bb9cb5b2d7e
Hayuna <marek.kowalonek@gmail.com>
2022-01-09 21:35:41 +0100

    Initial commit

---
+++ server/src/socket.ts
@@ -0,0 +1,13 @@
+import { useSocketServer } from 'socket-controllers'
+import { Server } from 'socket.io'
+
+export default (httpServer) => {
+    const io = new Server(httpServer, {
+        cors: {
+            origin: '*'
+        }
+    })
+
+    useSocketServer(io, { controllers: [__dirname + '/api/controllers/*.ts'] })
+    return io
+}
